aa9553967dcb5d6009d74d2e880c51f103129c02
Karim Sami <karimsami047@gmail.com>
2020-09-26 13:33:16 +0200

    adding a fully function form to take the user information on orders

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,29 +3,86 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import axiosOrders from '../../../axios/axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: { street: '', postalCode: '' },
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    placeholder: 'Your Name',
+                    type: 'text',
+                    name: 'name',
+                    label: 'Name',
+                },
+                value: '',
+            },
+
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    placeholder: 'Your address',
+                    type: 'text',
+                    name: 'address',
+                    label: 'Address',
+                },
+                value: '',
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    placeholder: 'ZIP Code',
+                    type: 'text',
+                    name: 'zipcode',
+                    label: 'ZIP Code',
+                },
+                value: '',
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    placeholder: 'Your Country',
+                    type: 'text',
+                    name: 'country',
+                    label: 'Country',
+                },
+                value: '',
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    placeholder: 'Your E-mail',
+                    type: 'email',
+                    name: 'mail',
+                    label: 'E-Mail',
+                },
+                value: '',
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: 'fastest', displayValue: 'Fastest' },
+                        { value: 'cheapest', displayValue: 'Cheapest' },
+                    ],
+                },
+                value: 'fastest',
+            },
+        },
         loading: false,
     };
 
     orderHandler = async (e) => {
         e.preventDefault();
         this.setState({ loading: true });
+        let formData = {};
+        for (let formElementID in this.state.orderForm) {
+            formData[formElementID] = this.state.orderForm[formElementID].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.totalPrice,
-            customer: {
-                name: 'Karim',
-                address: {
-                    street: 'test',
-                    zipCode: 35511,
-                    country: 'Egypt',
-                },
-                email: 'asfdas@asfsf.com',
-            },
+            orderData: formData,
         };
         try {
             const response = await axiosOrders.post('/orders.json', order);
@@ -36,18 +93,33 @@ class ContactData extends Component {
         }
         this.props.history.push('/');
     };
+    inputChangedHandler = (event, id) => {
+        const updatedOrderForm = { ...this.state.orderForm };
+        const updatedFormElement = { ...updatedOrderForm[id] };
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[id] = updatedFormElement;
+        this.setState({ orderForm: updatedOrderForm });
+    };
 
     render() {
         let form = (
-            <form>
-                <input type="text" name="name" placeholder="your name" />
-                <input type="email" name="email" placeholder="your email" />
-                <input type="text" name="street" placeholder="your street" />
-                <input
-                    type="text"
-                    name="postal"
-                    placeholder="your postal code"
-                />
+            <form onSubmit={this.orderHandler}>
+                {Object.keys(this.state.orderForm).map((key) => {
+                    return (
+                        <Input
+                            key={key}
+                            id={key}
+                            inputType={this.state.orderForm[key].elementType}
+                            elementConfig={
+                                this.state.orderForm[key].elementConfig
+                            }
+                            value={this.state.orderForm[key].value}
+                            changed={(event) =>
+                                this.inputChangedHandler(event, key)
+                            }
+                        />
+                    );
+                })}
 
                 <Button btnType="Success" clicked={this.orderHandler}>
                     ORDER
