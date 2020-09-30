bb4a36976d35eb87a68e55ae8b027db2b9d9b4f2
Karim Sami <karimsami047@gmail.com>
2020-10-01 00:31:49 +0200

    adding some tests

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -15,7 +15,7 @@ const INGREDIENT_PRICES = {
     meat: 1.3,
     bacon: 0.7,
 };
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     state = {
         purchasable: false,
         purchasing: false,
---
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import BuilControls from '../../components/Burger/BuildControls/BuildControls';
+import { BurgerBuilder } from './BurgerBuilder';
+
+configure({ adapter: new Adapter() });
+
+describe('<BurgerBuilder/>', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<BurgerBuilder onInitIngredients={() => {}} />);
+    });
+    it('should render <BuildControls /> when receiving ingredients', () => {
+        wrapper.setProps({ ings: { salad: 0 } });
+        expect(wrapper.find(BuilControls)).toHaveLength(1);
+    });
+});
